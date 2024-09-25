87060b221b0c4f3493e25970b2bc859ec65761cc
ifme18 <mainakenken188@gmail.com>
2024-09-25 13:33:15 +0300

    kk

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+// 1. saturdayFun function
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  
+  // 2. mondayWork function expression
+  const mondayWork = function(activity = 'go to the office') {
+    return `This Monday, I will ${activity}.`;
+  };
+  
+  // 3. wrapAdjective function
+  function wrapAdjective(wrapper = '*') {
+    return function(adjective = 'special') {
+      return `You are ${wrapper}${adjective}${wrapper}!`;
+    };
+  }
+  
